07d1fc8394274d82e460d34122b700352618cfd5
Huxley <huxhales@gmail.com>
2019-07-23 13:21:33 +1000

    Added model for listing and some basic code

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_22_051911) do
+ActiveRecord::Schema.define(version: 2019_07_23_030045) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "listings", force: :cascade do |t|
+    t.string "name"
+    t.decimal "price"
+    t.text "body"
+    t.boolean "alive"
+    t.string "category"
+    t.string "tag"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Listing.destroy_all
+
+
+Listing.create(name: "Test", price: 299.99, body: "Bunch of info about this project", alive: false, category: "website", tag: "saas", created_at: '07-04-2001')
+Listing.create(name: "Test2", price: 299.99, body: "Bunch of info about this project", alive: true, category: "website", tag: "saas", created_at: '07-04-2001')
+Listing.create(name: "Test3", price: 299.99, body: "Bunch of info about this project", alive: true, category: "website", tag: "saas", created_at: '07-04-2001')
+Listing.create(name: "Test4", price: 299.99, body: "Bunch of info about this project", alive: true, category: "website", tag: "saas", created_at: '07-04-2001')
+Listing.create(name: "Test5", price: 299.99, body: "Bunch of info about this project", alive: false, category: "website", tag: "saas", created_at: '07-04-2001')
+Listing.create(name: "Test6", price: 299.99, body: "Bunch of info about this project", alive: true, category: "website", tag: "saas", created_at: '07-04-2001')
