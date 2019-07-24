4df2a04df902335225a755405316aff59ea46f38
Huxley <huxhales@gmail.com>
2019-07-24 13:46:37 +1000

    Removed user controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   root to: 'app#index'
-  get 'user/dashboard', to: 'user#dashboard', as: 'user_dashboard'
+  get 'dashboard/dashboard', to: 'dashboard#dashboard', as: 'user_dashboard'
   get 'app/about', to: 'app#about', as: 'about'
   get 'app/show', to: 'app#show', as: 'show'
   post 'app/show', to: 'app#show', as: 'show_with_filters'
   get 'app/listing/:id', to: 'app#listing', as: 'listing'
-  get 'user/new_listing', to: 'user#new_listing', as: 'new_listing'
-  post 'user/create_listing', to: 'user#create_listing', as: 'create_listing'
+  get 'dashboard/new_listing', to: 'dashboard#new_listing', as: 'new_listing'
+  post 'dashboard/create_listing', to: 'dashboard#create_listing', as: 'create_listing'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
