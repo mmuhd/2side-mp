b6d50706dc308d0857da9b43dedce753efbdb527
Huxley <huxhales@gmail.com>
2019-07-24 15:33:27 +1000

    Added random seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,16 +8,14 @@
 
 Listing.destroy_all
 
-
-Listing.create(name: "Test", price: 299.99, body: "Bunch of info about this project", alive: false, category: "website", tag: "saas", created_at: '07-04-2001')
-Listing.create(name: "Test2", price: 399.99, body: "Bunch of info about this project", alive: true, category: "app", tag: "saas", created_at: '06-04-2001')
-Listing.create(name: "Test3", price: 899.99, body: "Bunch of info about this project", alive: true, category: "website", tag: "saas", created_at: '05-04-2001')
-Listing.create(name: "Test4", price: 99.99, body: "Bunch of info about this project", alive: true, category: "domain", tag: "saas", created_at: '04-04-2001')
-Listing.create(name: "Test5", price: 199.99, body: "Bunch of info about this project", alive: false, category: "website", tag: "saas", created_at: '07-04-2001')
-Listing.create(name: "Test6", price: 299.99, body: "Bunch of info about this project", alive: true, category: "app", tag: "saas", created_at: '03-04-2001')
-Listing.create(name: "Test7", price: 2329.99, body: "Bunch of info about this project", alive: false, category: "domain", tag: "saas", created_at: '02-04-2001')
-Listing.create(name: "Test8", price: 2.99, body: "Bunch of info about this project", alive: true, category: "website", tag: "saas", created_at: '08-04-2001')
-Listing.create(name: "Test9", price: 229.99, body: "Bunch of info about this project", alive: true, category: "website", tag: "saas", created_at: '11-04-2001')
-Listing.create(name: "Test10", price: 209.99, body: "Bunch of info about this project", alive: false, category: "website", tag: "saas", created_at: '09-04-2001')
-Listing.create(name: "Test11", price: 19.99, body: "Bunch of info about this project", alive: false, category: "website", tag: "saas", created_at: '010-04-2001')
-Listing.create(name: "Test12", price: 22.99, body: "Bunch of info about this project", alive: true, category: "app", tag: "saas", created_at: '20-04-2001')
+100.times do |db|
+db = Listing.new
+db.name = rand(10) > 5 ? "test world #{rand(10)}" : "test universe #{rand(50)}"
+db.body = "this is a body"
+db.price = rand(1.00..999.99)
+db.alive = true
+db.category = "domain"
+db.tag = "test_tag"
+db.created_at = Date.new(rand(1000..2077),rand(1..12),rand(1..30))
+db.save
+end
