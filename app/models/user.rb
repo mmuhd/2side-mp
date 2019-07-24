7c60de4193a4b756dc75971ddf03a8abcd2f7623
carlmccabe <carl.h.mccabe@gmail.com>
2019-07-24 11:21:59 +1000

    User Avatars Done

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_one_attached :avatar
+
   def name
     return username ? username : email
   end
