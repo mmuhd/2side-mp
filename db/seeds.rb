23a8a0fe7836be6e1c4da99164eb9213bf9bb2b7
Huxley <huxhales@gmail.com>
2019-07-29 10:21:51 +1000

    Fixed new listing maker

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,29 +7,13 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 Listing.destroy_all
-u = User.new
-u.username = "Test100"
-
-
-100.times do |db|
-db = Listing.new
-db.name = rand(10) > 5 ? "test world #{rand(10)}" : "test universe #{rand(50)}"
-db.body = "this is a body"
-db.price = rand(1.00..999.99)
-db.alive = true
-db.category = "domain"
-db.tag = "test_tag"
-db.created_at = Date.new(rand(1000..2077),rand(1..12),rand(1..30))
-db.save
-end
-
+User.destroy_all
 
 
 10.times do |db|
-
     db = User.new
     db.email = "user#{rand(1..10)}@test#{rand(0.1..10.00)}.com"
-    db.username = "Test#{rand(1.100):rand(1..100)}"
+    db.username = "Test #{rand(0.001..1.000)}"
     db.password = "Password"
     db.save
     5.times do |ls|
@@ -44,5 +28,4 @@ end
         ls.user = db
         ls.save
     end
-
 end
