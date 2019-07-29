dc2793cf36cf8d7cab1311666a89545e3e5b6751
Huxley <huxhales@gmail.com>
2019-07-29 10:03:53 +1000

    Adding refrences

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,9 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 Listing.destroy_all
+u = User.new
+u.username = "Test100"
+
 
 100.times do |db|
 db = Listing.new
@@ -18,4 +21,28 @@ db.category = "domain"
 db.tag = "test_tag"
 db.created_at = Date.new(rand(1000..2077),rand(1..12),rand(1..30))
 db.save
+end
+
+
+
+10.times do |db|
+
+    db = User.new
+    db.email = "user#{rand(1..10)}@test#{rand(0.1..10.00)}.com"
+    db.username = "Test#{rand(1.100):rand(1..100)}"
+    db.password = "Password"
+    db.save
+    5.times do |ls|
+        ls = Listing.new
+        ls.name = rand(10) > 5 ? "test world #{rand(10)}" : "test universe #{rand(50)}"
+        ls.body = "this is a body"
+        ls.price = rand(1.00..999.99)
+        ls.alive = true
+        ls.category = "domain"
+        ls.tag = "test_tag"
+        ls.created_at = Date.new(rand(1000..2077),rand(1..12),rand(1..30))
+        ls.user = db
+        ls.save
+    end
+
 end
